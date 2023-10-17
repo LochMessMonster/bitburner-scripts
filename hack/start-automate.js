84925fb8f44cd9dfe69ea30c0577029df7d43f64
LochMessMonster <lochmessmonster@localhost>
2023-10-17 22:17:02 +0100

    Sort targets by max money in descending

--- hack/start-automate.js
+++ hack/start-automate.js
@@ -12,22 +12,35 @@ export async function main(ns) {
         targetFilePath = ns.args[1];
         // defaultThreads = ns.args[2]
     }
-  
+
     // const targetList = ["hong-fang-tea", "foodnstuff", "sigma-cosmetics", "joesguns", "iron-gym", "harakiri-sushi", "phantasy", "n00dles"];
     const targetList = loadServerFile(ns, targetFilePath);
     const hostList = ns.getPurchasedServers().concat(targetList);
-  
+
     const hackScriptRam = ns.getScriptRam(scriptHack);
     let serverRamMax, numOfScriptsToRun;
-  
+
+    // Sort targetList by Server Money in descending
+    targetList.sort((srvA, srvB) => {
+      let moneyA = ns.getServerMoneyAvailable(srvA);
+      let moneyB = ns.getServerMoneyAvailable(srvB);
+      if (moneyA > moneyB) {
+        return -1;  // sort A before B
+      } else if (moneyA < moneyB) {
+        return 1;   // sort A after B
+      } else {
+        return 0;   // leave as-is
+      }
+    })
+
     // Loop through servers starting hack-server script for each srvr.
     for (let i = 0; i < hostList.length; i++) {
       serverRamMax = ns.getServerMaxRam(hostList[i]);
-  
+
       // number of scripts the server can run
       numOfScriptsToRun = Math.floor(serverRamMax / hackScriptRam)
       ns.printf("Server '%s' can run: %d", hostList[i], numOfScriptsToRun);
-  
+
       // if server can't run script, move to next
       if (numOfScriptsToRun <= 0) {
         continue;
@@ -36,12 +49,12 @@ export async function main(ns) {
         numOfScriptsToRun = targetList.length;
         ns.printf("Server '%s' set to run: %d", hostList[i], numOfScriptsToRun);
       }
-  
+
       // Upload hack script if not on server
       if (!ns.fileExists(scriptHack, hostList[i])) {
         ns.scp(scriptHack, hostList[i]);
       }
-  
+
       // start as many hack script as possible
       for (let j = 0; j < numOfScriptsToRun; j++) {
         ns.exec(scriptHack, hostList[i], defaultThreads, targetList[j]);
