20c71e760a3c6f90e31aa7c9482d99c4b56186e7
LochMessMonster <21160121+LochMessMonster@users.noreply.github.com>
2023-10-24 17:23:24 +0100

    remove home srv temporarily to prevent overhack@

--- src/scripts/hack/start-automate.ts
+++ src/scripts/hack/start-automate.ts
@@ -14,7 +14,7 @@ export async function main(ns: NS): Promise<void> {
   let nukedServers = loadServerFile(ns, Defaults.filepathTarget);
   // Server list of hosts (runs the scripts)
   let hostList = prepareServerHostList(ns, ns.getPurchasedServers().concat(nukedServers));
-  hostList.push(Defaults.home);
+  // hostList.push(Defaults.home);
 
   // Server list of targets (target of scripts)
   let targetList = prepareServerTargetList(ns, nukedServers);
