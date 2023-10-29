e1efd62bc3e0e521eac763f1eceffaab29bbf30d
LochMessMonster <lochmessmonster@localhost>
2023-10-29 18:13:56 +0000

    Add remove function for terminal

---
+++ src/scripts/terminal/remove.ts
@@ -0,0 +1,30 @@
+import { NS } from "@ns";
+
+import * as Defaults from "scripts/utils/defaults"
+
+// Merge this with below?
+export async function main(ns: NS): Promise<void> {
+    let path = <string> ns.args[0] ?? "";
+    ns.tprintf("Deleted: " + remove(ns, path));
+}
+
+/**
+ * Delete file or directory from current machine. Uses starting path 
+ * (e.g. `a/b/` or `a/b.txt`) to filter. Does not support wildcards.
+ * @param ns NS API
+ * @param path Path of file/directory from where to start deleting. Can't be empty. 
+ * @returns True if it successfully deletes, and false otherwise.
+ */
+function remove(ns:NS, path: string) : boolean {
+    let fileList = ns.ls(Defaults.home).filter(file => file.startsWith(path));
+
+    // If no files found or filepath is empty
+    if ((fileList.length < 1) || (path === "")) { return false; }
+
+    for (const file of fileList) {
+        ns.tprintf("Deleting %s", file);
+        let isDeleted = ns.rm(file);
+        if (!isDeleted) { return false;}
+    }
+    return true;
+}
